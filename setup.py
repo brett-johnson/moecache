a2e7fc268a5d148e659a99a1a59b554e5f5cf77c
Joe Bowers <joe@mixpanel.com>
2013-07-11 20:21:46 +0000

    Change version to "rolling", add note

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+# This file is provided as a convenience for
+# systems administrators, it does *not* represent
+# a release on pypi.
+
 from distutils.core import setup
 
-setup(name='memcache-client',
-      version='0.1',
+setup(name='memcache_client',
+      version='rolling',
       description='A minimal pure Python client for Memcached, Kestrel, etc.',
       url='https://github.com/mixpanel/memcache_client',
       py_modules=['memcache', 'mock_memcached', 'test'],
